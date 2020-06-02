60040eac39df62441523e548797744b24321c915
Ngon Phi <ngonphikp@gmail.com>
2020-06-02 16:59:08 +0700

    test json dynamic

--- Sources/application/views/admin/dashboard_admin_view.php
+++ Sources/application/views/admin/dashboard_admin_view.php
@@ -1,24 +1,28 @@
-<div class="row content">
+<?php
+ echo json_encode ($listQH, JSON_UNESCAPED_UNICODE);
+?>
+
+<!-- <div class="row content">
     <div class="col-lg-3 col-md-6 me-db-item" style="background: linear-gradient(214.27deg, rgba(189, 33, 48, 0.5) 24.07%, rgba(255, 255, 255, 0) 100%), #ADD8E6;">
         <h4 class="text-right">Tổng Số thành viên</h4>
-        <h4 class="text-right"><?php echo $countUser;?></h4>
+        <h4 class="text-right"><?php //echo $countUser;?></h4>
         <p class="text-left"><i class="fa fa-user fa-4x" style="color: #bd2130" aria-hidden="true"></i></p>
     </div>
     <div class="col-lg-3 col-md-6 me-db-item" style="background: linear-gradient(214.27deg, rgba(0, 0, 255, 0.5) 24.07%, rgba(255, 255, 255, 0) 100%), #FFFFE0;">
         <h4 class="text-right">Tổng Số Địa Điểm Du Lịch</h4>
-        <h4 class="text-right"><?php echo $countDD;?></h4>
+        <h4 class="text-right"><?php //echo $countDD;?></h4>
         <p class="text-left"><i class="fa fa-table fa-4x" style="color: blue" aria-hidden="true"></i></p>
     </div>
     <div class="col-lg-3 col-md-6 me-db-item" style="background: linear-gradient(214.27deg, rgba(205, 110, 0, 0.5) 24.07%, rgba(255, 255, 255, 0) 100%), #90EE90;">
         <h4 class="text-right">Tổng Số Dịch Vụ</h4>
-        <h4 class="text-right"><?php echo $countDV;?></h4>
+        <h4 class="text-right"><?php //echo $countDV;?></h4>
         <p class="text-left"><i class="fa fa-newspaper-o fa-4x" style="color: #cd6e00" aria-hidden="true"></i></p>
     </div>
     <div class="col-lg-3 col-md-6 me-db-item" style="background: linear-gradient(214.27deg, rgba(255, 255, 255, 0.5) 24.07%, rgba(255, 255, 255, 0) 100%), #FFDEAD;">
         <h4 class="text-right">Tổng Số Cẩm Nang</h4>
-        <h4 class="text-right"><?php echo $countCN;?></h4>
+        <h4 class="text-right"><?php //echo $countCN;?></h4>
         <p class="text-left"><i class="fa fa-newspaper-o fa-4x" style="color: white" aria-hidden="true"></i></p>
-    </div>
+    </div> -->
 
     <div class="row">
           <div class="col-lg-6">
