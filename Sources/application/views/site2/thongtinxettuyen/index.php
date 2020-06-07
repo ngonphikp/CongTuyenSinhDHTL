5f26a3340d9d92f50a0126f5b5476894def6d22a
lxdat98 <datlx62@wru.vn>
2020-06-07 16:19:47 +0700

    Nothing

--- Sources/application/views/site2/thongtinxettuyen/index.php
+++ Sources/application/views/site2/thongtinxettuyen/index.php
@@ -17,6 +17,7 @@
     <div id="wrapper">
         <?php 
            include( APPPATH.'views/site2/home/header.php');
+           include( APPPATH.'views/site2/home/slider.php');
            include( APPPATH.'views/site2/home/nav-bot.php');
            include( APPPATH.'views/site2/thongtinxettuyen/main.php');
            include( APPPATH.'views/site2/home/footer.php');
